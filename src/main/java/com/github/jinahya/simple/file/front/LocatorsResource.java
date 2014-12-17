13b94d8108e881bc29be005f3a1a68cf2f5bb4e7
Jin Kwon <onacit@gmail.com>
2014-12-17 20:52:02 +0900

    updated

--- src/main/java/com/github/jinahya/simple/file/front/LocatorsResource.java
+++ src/main/java/com/github/jinahya/simple/file/front/LocatorsResource.java
@@ -22,22 +22,38 @@ import com.github.jinahya.simple.file.back.DefaultFileContext;
 import com.github.jinahya.simple.file.back.FileBack;
 import com.github.jinahya.simple.file.back.FileBackException;
 import com.github.jinahya.simple.file.back.FileContext;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import static java.lang.invoke.MethodHandles.lookup;
+import java.net.URI;
 import java.nio.ByteBuffer;
-import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.Optional;
+import javax.annotation.PreDestroy;
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -46,6 +62,7 @@ import javax.ws.rs.core.UriInfo;
 import org.apache.commons.lang3.mutable.Mutable;
 import org.apache.commons.lang3.mutable.MutableLong;
 import org.apache.commons.lang3.mutable.MutableObject;
+import org.glassfish.jersey.client.ClientProperties;
 import org.slf4j.Logger;
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -58,153 +75,333 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class LocatorsResource {
 
 
-    /**
-     * Reads file content mapped to specified {@code locator}.
-     *
-     * @param locator the locator of file.
-     *
-     * @return a response.
-     */
-    @Consumes(MediaType.APPLICATION_OCTET_STREAM)
-    @GET
-    @Path("/{locator: .+}")
-    public StreamingOutput readSingle(
-        @PathParam("locator") final String locator) {
+    private static final String PROPERTY_PREFIX
+        = "http://jinahya.github.com/simple/file/front";
 
-        final FileContext fileContext = new DefaultFileContext();
 
-        fileContext.keyBufferSupplier(
-            () -> ByteBuffer.wrap(locator.getBytes(StandardCharsets.UTF_8)));
+    private static final String PROPERTY_TEMP_PATH
+        = PROPERTY_PREFIX + "/temp_path";
 
-        final Mutable<java.nio.file.Path> localPathHolder
-            = new MutableObject<>();
-        fileContext.localPathConsumer(localPath -> {
-            localPathHolder.setValue(localPath);
-            servletResponse.setStatus(
-                Files.isRegularFile(localPath)
-                ? HttpServletResponse.SC_OK : HttpServletResponse.SC_NOT_FOUND);
-        });
 
-        final Mutable<String> pathNameHolder = new MutableObject<>();
-        fileContext.pathNameConsumer(pathName -> {
-            servletResponse.setHeader(
-                FileFrontConstants.HEADER_PATH_NAME, pathName);
-            pathNameHolder.setValue(pathName);
-        });
+    private static final String PROPERTY_MEDIA_TYPE
+        = PROPERTY_PREFIX + "/media_type";
 
-        final MutableLong bytesCopiedHolder = new MutableLong();
-        fileContext.bytesCopiedConsumer(bytesCopied -> {
-            bytesCopiedHolder.setValue(bytesCopied);
-            servletResponse.setHeader(
-                FileFrontConstants.HEADER_BYTES_COPIED,
-                Long.toString(bytesCopied));
-        });
 
-        return targetStream -> {
-            fileContext.targetChannelSupplier(
-                () -> Channels.newChannel(targetStream));
-            try {
-                fileBack.read(fileContext);
-            } catch (final FileBackException fbe) {
-                throw new WebApplicationException(fbe);
+    public static void updateSingleDistribute(final FileContext fileContext,
+                                              final UriInfo uriInfo,
+                                              final List<URI> fileFronts) {
+
+        final Logger logger = getLogger(lookup().lookupClass());
+
+        final URI baseUri = uriInfo.getBaseUri();
+        logger.debug("baseUri: {}", baseUri);
+        final String path = uriInfo.getPath();
+        logger.debug("path: {}", path);
+
+        final java.nio.file.Path tempPath = fileContext.property(
+            PROPERTY_TEMP_PATH, java.nio.file.Path.class).orElse(null);
+        logger.debug("tempPath: {}", tempPath);
+        if (tempPath == null) {
+            logger.error("no tempPath supplied");
+            return;
+        }
+        final MediaType mediaType = fileContext.property(
+            PROPERTY_MEDIA_TYPE, MediaType.class)
+            .orElse(MediaType.APPLICATION_OCTET_STREAM_TYPE);
+        logger.debug("mediaType: {}", mediaType);
+
+        for (final URI fileFront : fileFronts) {
+            if (baseUri.equals(fileFront)) {
+                logger.debug("skipping self: " + fileFront);
+                continue;
             }
-        };
+            final Client client = ClientBuilder.newClient();
+            WebTarget target = client.target(fileFront).path(path)
+                .queryParam("distribute", Boolean.FALSE.toString());
+            final String suffix = Optional.ofNullable(
+                fileContext.fileSuffixSupplier()).orElse(() -> null)
+                .get();
+            if (suffix != null && !suffix.trim().isEmpty()) {
+                target = target.queryParam("suffix", suffix);
+            }
+            logger.debug("target: {}", target.getUri().toString());
+            try {
+                final Entity<File> entity
+                    = Entity.entity(tempPath.toFile(), mediaType);
+                final Response response = target.request().put(entity);
+                logger.debug("status: {}", response.getStatusInfo());
+            } catch (ProcessingException pe) {
+                logger.error("failed to distribute to " + fileFront, pe);
+            }
+        }
+    }
+
+
+    public static void deleteSingleDistribute(final FileContext fileContext,
+                                              final UriInfo uriInfo,
+                                              final List<URI> fileFronts) {
+
+        final Logger logger = getLogger(lookup().lookupClass());
+
+        final URI baseUri = uriInfo.getBaseUri();
+        logger.debug("baseUri: {}", baseUri);
+        final String path = uriInfo.getPath();
+        logger.debug("path: {}", path);
+
+        for (final URI fileFront : fileFronts) {
+            if (baseUri.equals(fileFront)) {
+                logger.debug("skipping self: " + fileFront);
+                continue;
+            }
+            final Client client = ClientBuilder.newClient()
+                .property(ClientProperties.CONNECT_TIMEOUT, 1000)
+                .property(ClientProperties.READ_TIMEOUT, 1000);
+            WebTarget target = client.target(fileFront).path(path)
+                .queryParam("distribute", Boolean.FALSE.toString());
+            final String suffix = Optional.ofNullable(
+                fileContext.fileSuffixSupplier()).orElse(() -> null)
+                .get();
+            if (suffix != null && !suffix.trim().isEmpty()) {
+                target = target.queryParam("suffix", suffix);
+            }
+            logger.debug("target: {}", target.getUri().toString());
+            try {
+                final Response response = target.request().delete();
+                logger.debug("status: {}", response.getStatusInfo());
+            } catch (final ProcessingException pe) {
+                logger.error("failed to distribute to " + fileFront, pe);
+            }
+        }
+    }
+
+
+    @PreDestroy
+    public void preDestoy() {
+
+        if (tempPath != null) {
+            try {
+                Files.delete(tempPath);
+            } catch (final IOException ioe) {
+                logger.error("failed to delete temp path: " + tempPath, ioe);
+            }
+        }
     }
 
 
     /**
-     * Updates a file content mapped to specified {@code locator}.
+     * Reads file content mapped to specified {@code locator}.
      *
      * @param locator the locator of file.
+     * @param suffix an optional file name suffix
      *
      * @return a response.
      */
-    @PUT
+    @Consumes(MediaType.WILDCARD)
+    @GET
     @Path("/{locator: .+}")
-    @Produces(MediaType.APPLICATION_OCTET_STREAM)
-    public Response updateSingle(
-        @PathParam("locator") final String locator) {
+    public Response readSingle(
+        @PathParam("locator") final String locator,
+        @QueryParam("suffix") final String suffix) {
 
         final FileContext fileContext = new DefaultFileContext();
 
         fileContext.keyBufferSupplier(
             () -> ByteBuffer.wrap(locator.getBytes(StandardCharsets.UTF_8)));
 
-        final Mutable<java.nio.file.Path> localPathHolder
-            = new MutableObject<>();
-        fileContext.localPathConsumer(localPath -> {
-            localPathHolder.setValue(localPath);
-//            servletResponse.setStatus(
-//                Files.isRegularFile(localPath)
-//                ? HttpServletResponse.SC_OK : HttpServletResponse.SC_CREATED);
-        });
+        if (suffix != null && !suffix.trim().isEmpty()) {
+            fileContext.fileSuffixSupplier(() -> suffix.trim());
+        }
 
-        final Mutable<String> pathNameHolder = new MutableObject<>();
-        fileContext.pathNameConsumer(pathName -> {
-            pathNameHolder.setValue(pathName);
-            servletResponse.setHeader(
-                FileFrontConstants.HEADER_PATH_NAME, pathName);
-        });
-
-        fileContext.sourceChannelSupplier(() -> {
+        fileContext.targetChannelSupplier(() -> {
             try {
-                return Channels.newChannel(servletRequest.getInputStream());
+                tempPath = Files.createTempFile("prefix", "suffix");
+                return FileChannel.open(tempPath, StandardOpenOption.READ,
+                                        StandardOpenOption.WRITE);
             } catch (final IOException ioe) {
                 throw new RuntimeException(ioe);
             }
         });
 
-        final MutableLong bytesCopiedHolder = new MutableLong();
-        fileContext.bytesCopiedConsumer(
-            bytesCopied -> bytesCopiedHolder.setValue(bytesCopied)
-        );
+        final MutableLong targetCopiedHolder = new MutableLong();
+        fileContext.targetCopiedConsumer(targetCopied -> {
+            targetCopiedHolder.setValue(targetCopied);
+        });
+
+        final Mutable<String> pathNameHolder = new MutableObject<>();
+        fileContext.pathNameConsumer(pathName -> {
+            pathNameHolder.setValue(pathName);
+        });
 
         try {
-            fileBack.write(fileContext);
+            fileBack.read(fileContext);
         } catch (final IOException | FileBackException e) {
-            throw new WebApplicationException(e); // 500
+            throw new WebApplicationException(e);
         }
 
-        return Response.noContent().build();
+        if (targetCopiedHolder.getValue() < 0L) {
+            throw new NotFoundException();
+        }
+
+        return Response.ok((StreamingOutput) (output) -> {
+            Files.copy(tempPath, output);
+        })
+            .header("Content-Length", targetCopiedHolder.getValue())
+            .header(FileFrontConstants.HEADER_PATH_NAME,
+                    pathNameHolder.getValue())
+            .build();
     }
 
 
-    @DELETE
+    /**
+     * Updates a file content located by specified {@code locator}.
+     *
+     * @param locator the locator of file.
+     * @param suffix file suffix
+     * @param distribute a flag for distributing to siblings.
+     * @param entity
+     *
+     * @return a response.
+     */
+    @PUT
     @Path("/{locator: .+}")
-    public Response deleteSingle(@PathParam("locator") final String locator) {
+    @Produces(MediaType.WILDCARD)
+    public Response updateSingle(
+        @PathParam("locator") final String locator,
+        @QueryParam("suffix") final String suffix,
+        @QueryParam("distribute")
+        @DefaultValue("true") final boolean distribute,
+        final InputStream entity) {
 
         final FileContext fileContext = new DefaultFileContext();
 
         fileContext.keyBufferSupplier(
             () -> ByteBuffer.wrap(locator.getBytes(StandardCharsets.UTF_8)));
 
+        if (suffix != null && !suffix.trim().isEmpty()) {
+            fileContext.fileSuffixSupplier(() -> suffix.trim());
+        }
+
+        final Mutable<java.nio.file.Path> localPathHolder
+            = new MutableObject<>();
+        fileContext.localLeafConsumer(
+            localPath -> localPathHolder.setValue(localPath)
+        );
+
+        final Mutable<String> pathNameHolder = new MutableObject<>();
+        fileContext.pathNameConsumer(pathName -> {
+            pathNameHolder.setValue(pathName);
+        });
+
         try {
-            fileBack.delete(fileContext);
-        } catch (final IOException | FileBackException e) {
-            throw new WebApplicationException(e);
+            tempPath = Files.createTempFile("prefix", "suffix");
+            Files.copy(entity, tempPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (final IOException ioe) {
+            throw new WebApplicationException(ioe);
+        }
+
+        fileContext.sourceChannelSupplier(() -> {
+            try {
+                return FileChannel.open(tempPath, StandardOpenOption.READ);
+            } catch (final IOException ioe) {
+                throw new RuntimeException(ioe);
+            }
+        });
+
+        final MutableLong sourceCopiedHolder = new MutableLong();
+        fileContext.sourceCopiedConsumer(
+            sourceCopied -> sourceCopiedHolder.setValue(sourceCopied)
+        );
+
+        try {
+            fileBack.write(fileContext);
+        } catch (IOException | FileBackException e) {
+            logger.error("failed to write", e);
+            throw new WebApplicationException(e); // 500
+        }
+
+        if (distribute) {
+            logger.debug("distrubuting...");
+            fileContext.property(PROPERTY_TEMP_PATH, tempPath);
+            fileContext.property(PROPERTY_MEDIA_TYPE, contentType);
+            updateSingleDistribute(fileContext, uriInfo, fileFronts);
         }
 
         return Response.noContent().build();
     }
 
 
-    private final transient Logger logger = getLogger(getClass());
+    /**
+     * Deletes a file content located by given {@code locator}.
+     *
+     * @param locator the file locator
+     * @param suffix an optional file name suffix such as "png" or "txt"
+     * @param distribute
+     *
+     * @return response
+     */
+    @DELETE
+    @Path("/{locator: .+}")
+    public Response deleteSingle(
+        @PathParam("locator") final String locator,
+        @QueryParam("suffix") final String suffix,
+        @QueryParam("distribute")
+        @DefaultValue("true") final boolean distribute) {
+
+        final FileContext fileContext = new DefaultFileContext();
+
+        fileContext.keyBufferSupplier(
+            () -> ByteBuffer.wrap(locator.getBytes(StandardCharsets.UTF_8)));
+
+        if (suffix != null && !suffix.trim().isEmpty()) {
+            fileContext.fileSuffixSupplier(() -> suffix.trim());
+        }
+
+        try {
+            fileBack.delete(fileContext);
+        } catch (IOException | FileBackException e) {
+            logger.error("failed to delete", e);
+            throw new WebApplicationException(e);
+        }
+
+        if (distribute) {
+            logger.debug("distributing..");
+            deleteSingleDistribute(fileContext, uriInfo, fileFronts);
+        }
+
+        return Response.noContent().build();
+    }
 
 
+    private transient Logger logger = getLogger(getClass());
+
+
+    private transient java.nio.file.Path tempPath;
+
+
+    /**
+     * A file back injected.
+     */
     @Inject
-    protected FileBack fileBack;
+    private FileBack fileBack;
 
 
+    /**
+     * A list of sibling file fronts to distribute files and commands.
+     */
+    @Inject
+    private List<URI> fileFronts;
+
+
+    //@Context
+    //private HttpServletRequest servletRequest;
+    //@Context
+    //private HttpServletResponse servletResponse;
     @Context
-    protected HttpServletRequest servletRequest;
+    private UriInfo uriInfo;
 
 
-    @Context
-    protected HttpServletResponse servletResponse;
-
-
-    @Context
-    protected UriInfo uriInfo;
+    @HeaderParam("Content-Type")
+    private MediaType contentType;
 
 
 }
